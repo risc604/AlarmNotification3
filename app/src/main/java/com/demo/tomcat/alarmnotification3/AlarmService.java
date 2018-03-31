152b7833c6eb535eee43bf845aa3c64c7d047ff5
tomcat <tomcat.cheng@microlife.com.tw>
2018-03-31 11:12:11 +0800

    add code.

--- app/src/main/java/com/demo/tomcat/alarmnotification3/AlarmService.java
+++ app/src/main/java/com/demo/tomcat/alarmnotification3/AlarmService.java
@@ -1,16 +1,79 @@
 package com.demo.tomcat.alarmnotification3;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
-public class AlarmService extends Service {
-    public AlarmService() {
+public class AlarmService extends Service
+{
+    private static final String TAG = AlarmService.class.getSimpleName();
+    private final IBinder binder = new ServiceBinder();
+    NotificationManager     alarmNotificationManager;
+
+    public AlarmService()
+    {
+        Log.w(TAG, "AlarmService, constructor. ");
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
+    public IBinder onBind(Intent intent)
+    {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        // throw new UnsupportedOperationException("Not yet implemented");
+        Log.w(TAG, "onBind(), ");
+
+        return binder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        sendNotification("Wake Up! Wake Up!");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+
+    //---------------------- user function -----------------------//
+    private void sendNotification(String msg)
+    {
+        Log.w(TAG, "sendNotification(), ");
+
+        if (alarmNotificationManager == null) {
+            alarmNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+            PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                    new Intent(this, MainActivity.class), 0);
+
+            NotificationCompat.Builder alarmNotificationBuilder = new NotificationCompat.Builder(this)
+                    .setContentTitle("Alarm")
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+                    .setContentText(msg);
+
+            alarmNotificationBuilder.setContentIntent(contentIntent);
+            alarmNotificationManager.notify(1, alarmNotificationBuilder.build());
+            Log.d(TAG, "Notification sent.");
+        }
+        else
+        {
+            alarmNotificationManager.cancelAll();
+        }
+    }
+
+
+
+    //---------------------- inner class -----------------------//
+    public class ServiceBinder extends Binder
+    {
+        AlarmService getService()
+        {
+            Log.w(TAG, "getAlarmService(), ");
+            return AlarmService.this;
+        }
     }
 }
--- app/src/main/java/com/demo/tomcat/alarmnotification3/MainActivity.java
+++ app/src/main/java/com/demo/tomcat/alarmnotification3/MainActivity.java
@@ -3,9 +3,15 @@ package com.demo.tomcat.alarmnotification3;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,10 +22,13 @@ import android.widget.ToggleButton;
 
 import java.util.Calendar;
 
+import static android.support.v4.content.WakefulBroadcastReceiver.startWakefulService;
+
 public class MainActivity extends AppCompatActivity
 {
     private static final String TAG = MainActivity.class.getSimpleName();
-    private final String ALARM_TIMER = this.getPackageName() + ".ALARM_TIMER_ACTION";
+    private static final String ALARM_TIMER = "com.demo.tomcat.alarmnotification3.ACTION_ALARM_TIMER";
+    private static final String NOTIFICATION = "com.demo.tomcat.alarmnotification3.ACTION_NOTIFICATION";
 
 
     TimePicker alarmTimerPicker;
@@ -28,32 +37,101 @@ public class MainActivity extends AppCompatActivity
 
     AlarmManager    alarmManager;
     PendingIntent   pendingIntent;
+    AlarmReceiver   alarmReceiver;
+    AlarmService    alarmService;
+
+    private static boolean  registedFilter = false;
 
     public class AlarmReceiver extends BroadcastReceiver
     {
+        Ringtone ringtone;
+
         @Override
         public void onReceive(Context context, Intent intent)
         {
+            Log.w(TAG, "onServiceConnected(), ");
             String actionEvent = intent.getAction();
             if (actionEvent.equalsIgnoreCase(ALARM_TIMER))
             {
+                Log.w(TAG, "ACTION ALARM_TIMER, ");
+                Uri alarmUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+                if (alarmUri == null) {
+                    //alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+                    alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                }
+
+                ringtone = RingtoneManager.getRingtone(context, alarmUri);
+                ringtone.play();
+
+                ComponentName comp = new ComponentName(context.getPackageName(), AlarmService.class.getName());
+                startWakefulService(context, (intent.setComponent(comp)));
 
             }
-            else if (actionEvent.equalsIgnoreCase(""))
+            else if (actionEvent.equalsIgnoreCase(NOTIFICATION))
             {
+                Log.w(TAG, "ACTION NOTIFICATION, ");
 
             }
         }
     }
 
+    private ServiceConnection mConnection = new ServiceConnection()
+    {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service)
+        {
+            Log.w(TAG, "onServiceConnected(), ");
+            alarmService = ((AlarmService.ServiceBinder) service).getService();
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name)
+        {
+            Log.w(TAG, "onServiceDisconnected(), ");
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.w(TAG, "onCreate(), ");
 
         initView();
         initControl();
-        makeFilter();
+
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.w(TAG, "onStart(), mConnection: " + mConnection);
+
+        if (!registedFilter)
+        {
+            alarmReceiver = new AlarmReceiver();
+            registerReceiver(alarmReceiver, makeFilterRegister());
+            registedFilter = true;
+        }
+
+        //if (mConnection == null)
+        //{
+            Intent serviceIntent = new Intent(this, AlarmService.class);
+            bindService(serviceIntent, mConnection, BIND_AUTO_CREATE);
+        //}
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.w(TAG, "onStop(), ");
+
+        if (registedFilter)
+            unregisterReceiver(alarmReceiver);
     }
 
     public void onToggleClicked(View view)
@@ -98,13 +176,19 @@ public class MainActivity extends AppCompatActivity
 
     public void setAlarmText(String alarmText)
     {
+        Log.w(TAG, " setAlarmText(), ");
         alarmTextView.setText(alarmText);
     }
 
-    public void makeFilter()
+    private static IntentFilter makeFilterRegister()
     {
-        IntentFilter filter = new IntentFilter();
+        Log.w(TAG, " makeFilterRegister(), ");
+        final IntentFilter filter = new IntentFilter();
         filter.addAction(ALARM_TIMER);
+        filter.addAction(NOTIFICATION);
+        return filter;
+
+
 
     }
 
